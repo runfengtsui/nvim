e3a3d641526558731cd196b3b2f259a17847e0a1
Runfeng Tsui <runfengtsui@yeah.net>
2024-09-07 15:21:57 +0800

    use Powershell in toggleterm.nvim

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,20 +1,41 @@
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
-  opts = {
-    --[[ things you want to change go here]]
-    size = function(term)
-      if term.direction == "horizontal" then
-        return 9
-      elseif term.direction == "vertical" then
-        return vim.o.columns * 0.45
+  config = function ()
+    -- https://github.com/akinsho/toggleterm.nvim/wiki/Tips-and-Tricks#using-toggleterm-with-powershell
+    local powershell_options = {
+      shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+      shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+      shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+      shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+      shellquote = "",
+      shellxquote = "",
+    }
+    for option, value in pairs(powershell_options) do
+      vim.opt[option] = value
+    end
+
+    local myshell = function ()
+      if vim.loop.os_uname().sysname == "Windows_NT" then
+        return vim.o.shell
+      elseif vim.loop.os_uname().sysname == "Linux" then
+        return "/usr/local/bin/fish"
       end
-    end,
-    open_mapping = [[<c-\>]],
-    start_in_insert = true,
-    direction = "horizontal",
-    -- change the default shell to fish
-    shell = "/usr/local/bin/fish"
-  },
+    end
+
+    require("toggleterm").setup({
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 15
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.45
+        end
+      end,
+      open_mapping = [[<c-\>]],
+      start_in_insert = true,
+      direction = "horizontal",
+      shell = myshell,
+    })
+  end
 }
 
