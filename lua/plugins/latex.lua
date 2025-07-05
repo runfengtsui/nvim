2634b5197e4bbfcc394b263254fbfbf433a00801
Runfeng Tsui <runfengtsui@yeah.net>
2025-07-05 17:20:31 +0800

    use okular not zathura as the PDF viewer

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -1,7 +1,13 @@
 return {
   "lervag/vimtex",
+  lazy = false,
   ft = "tex",
   config = function ()
+    -- Viewer Settings
+    if vim.fn.executable("okular") then
+      vim.g.vimtex_view_general_viewer = 'okular'
+      vim.g.vimtex_view_general_options = '--unique file:@pdf#src:@line@tex'
+    end
     -- 设置撰写的 Tex 文档时 LaTeX 语法风格的文档
     vim.g.tex_flavor = 'latex'
     vim.g.vimtex_compiler_latexmk = {
@@ -15,8 +21,6 @@ return {
     }
     -- 不自动弹出错误窗口, 可以使用 :copen 手动打开错误窗口
     vim.g.vimtex_quickfix_mode = 0
-    -- 指定所使用的 PDF 阅读器
-    vim.g.vimtex_view_method = "zathura"
     -- 这一项目默认即为 nvr，但是如果由于种种原因无法实现 SyncTeX 同步位置，可以考虑手动指定这一项目
     -- 同时需要安装 neovim-remote
     vim.g.vimtex_compiler_progname = 'nvr'
