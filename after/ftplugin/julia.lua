cd992710dfa4dbdc8a6e29cdbff1ec03ba858883
Runfeng Tsui <runfengtsui@yeah.net>
2025-11-29 13:41:48 +0800

    feat: Run julia command in local environment

--- after/ftplugin/julia.lua
+++ after/ftplugin/julia.lua
@@ -3,19 +3,27 @@ vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
 vim.opt_local.softtabstop = 4
 
--- custom julia terminal
+-- Get Julia command if local environment exists.
+local function get_julia_command()
+  local command = 'julia '
+  if vim.fn.filereadable('Project.toml') then
+    command = command .. '--project=' .. vim.fn.getcwd() .. ' '
+  end
+  return command
+end
+
+-- Custom Julia terminal using toggleterm.nvim plugin.
 vim.keymap.set("n", "<leader>t", function ()
   local Terminal = require('toggleterm.terminal').Terminal
   local juterm = Terminal:new({
-    cmd = 'julia',
+    cmd = get_julia_command(),
     direction = 'vertical',
   })
   juterm:toggle()
 end, { noremap = true, silent = true })
 
--- Run Julia file
-vim.keymap.set("n", "<F5>", function ()
-  local command = "TermExec cmd='julia " .. vim.fn.expand('%') .. "'"
-  vim.api.nvim_command(command)
+-- Run Julia file.
+vim.keymap.set("n", "<leader>r", function ()
+  local command = get_julia_command() .. vim.fn.expand('%')
+  vim.cmd('!' .. command)
 end, { noremap = true, silent = true })
-
